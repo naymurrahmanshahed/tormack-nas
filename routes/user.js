af13016f3374006f2f78ea300a8674d092f89dcd
Naymur Rahman <naymurrahmanshahed@gmail.com>
2023-06-08 18:20:26 +0600

    auth middleWares added

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,14 @@
 const express = require("express");
 
-const { signupUser, loginUser } = require("../controllers/user");
+const {
+  signupUser,
+  loginUser,
+  getUser,
+  getUsers,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 //router
 const router = express.Router();
@@ -11,5 +19,17 @@ router.post("/login", loginUser);
 
 //signup route
 router.post("/signup", signupUser);
+
+//get an User
+router.get("/:userId", authMiddleware, getUser);
+
+//get all user
+router.get("/all", authMiddleware, getUsers);
+
+//update an user
+router.patch("/:userId", authMiddleware, updateUser);
+
+//delete an user
+router.delete("/:userId", authMiddleware, deleteUser);
 
 module.exports = router;

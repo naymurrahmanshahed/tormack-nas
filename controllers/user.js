24b209c7c0d44fb30e00d13e97490b9c51edff7e
Naymur Rahman <naymurrahmanshahed@gmail.com>
2023-06-13 15:41:41 +0600

    update user fixed

--- controllers/user.js
+++ controllers/user.js
@@ -126,9 +126,11 @@ const updateUser = async (req, res) => {
       { $set: { name } },
       { new: true }
     );
+
     if (!user) {
       throw Error("User Not Found");
     }
+    res.status(200).json(user);
   } catch (err) {
     res.status(404).json({ error: err.message });
   }
